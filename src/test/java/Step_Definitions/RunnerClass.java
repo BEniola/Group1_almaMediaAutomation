5bf116bf0050e0a8bb7a83a8c3771abf26063d9f
Bosede <bosedeeniola@gmail.com>
2023-12-03 01:31:31 +0100

    RunnerClass

---
+++ src/test/java/Step_Definitions/RunnerClass.java
@@ -0,0 +1,15 @@
+package Step_Definitions;
+
+import Base.TestBase;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@CucumberOptions(strict = false,
+        features = "src/test/resources",
+        plugin = {"pretty", "json:target/cucumber.json"},
+        tags = {"~@ignore"} )
+@RunWith(Cucumber.class)
+
+public class RunnerClass extends TestBase {
+}
